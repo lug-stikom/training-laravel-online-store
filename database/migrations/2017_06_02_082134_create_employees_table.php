f7fccdffd84f6a683d7038383f1216c1b048387b
R. Kukuh <rkukuh@gmail.com>
2017-06-02 17:08:07 +0700

    Create and initialize new migration

---
+++ database/migrations/2017_06_02_082134_create_employees_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateEmployeesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('employees', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('nik')->unique();
+            $table->string('nama');
+            $table->string('email')->unique();
+            $table->date('tanggal_lahir');
+            $table->double('gaji_pokok');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('employees');
+    }
+}
