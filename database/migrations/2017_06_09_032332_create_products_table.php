b9311237eb47d76742ae92b120eefdbd02a1e3c7
R. Kukuh <rkukuh@gmail.com>
2017-06-04 10:26:40 +0700

    Create new migration for product

---
+++ database/migrations/2017_06_09_032332_create_products_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateProductsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('products', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('sku')->unique();
+            $table->string('name')->unique();
+            $table->integer('parent_id')->unsigned()->nullable();
+            $table->double('unit_price');
+            $table->double('sale_price')->default(0);
+            $table->stock('integer');
+            $table->text('description');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('products');
+    }
+}
