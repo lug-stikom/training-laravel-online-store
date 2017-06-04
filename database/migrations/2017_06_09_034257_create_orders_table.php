b520e416d488da1e6ec78b963d1cb49f811bebcb
R. Kukuh <rkukuh@gmail.com>
2017-06-04 10:46:28 +0700

    Create migration for order

---
+++ database/migrations/2017_06_09_034257_create_orders_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateOrdersTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('orders', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('order_no')->unique();
+            $table->integer('user_id')->unsigned();
+            $table->integer('gateway_id')->unsigned();
+            $table->double('grand_total');
+            $table->text('notes')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('orders');
+    }
+}
