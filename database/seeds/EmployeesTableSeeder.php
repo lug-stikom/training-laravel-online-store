d1da07f7500a09741c670aad9c91b8c5f6edc577
R. Kukuh <rkukuh@gmail.com>
2017-06-04 11:31:47 +0700

    Create seeder for employee

---
+++ database/seeds/EmployeesTableSeeder.php
@@ -0,0 +1,25 @@
+<?php
+
+use Carbon\Carbon;
+use Illuminate\Database\Seeder;
+
+class EmployeesTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        DB::table('employees')->insert([
+            'nik' => 'N-0001',
+            'nama' => 'Kukuh',
+            'email' => 'rkukuh@gmail.com',
+            'tanggal_lahir' => Carbon::now()->->setDate(1987, 1, 31)
+            'gaji_pokok' => 10000000,
+            'created_at' => new DateTime(),
+            'updated_at' => new DateTime(),
+        ]);
+    }
+}
