6aa39002e6c5a03d3e92eb3429a61bc2c2677e3d
R. Kukuh <rkukuh@gmail.com>
2017-07-11 09:16:21 +0700

    Call seeder for relation between Category & Product

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -19,5 +19,6 @@ class DatabaseSeeder extends Seeder
 
         $this->call(CategoriesTableSeeder::class);
         $this->call(ProductsTableSeeder::class);
+        $this->call(CategoryProductTableSeeder::class);
     }
 }
